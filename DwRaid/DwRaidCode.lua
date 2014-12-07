0a0a9cb3aa926272481d782f816e82e8e22f445f
lilrichy <richardreigens@gmail.com>
2014-12-07 11:55:43 -0500

    v1.1.5
    Some Code Clean up.
    Fixed Personal Loot option - For real this time...
    Added chat messages for loot rules and voice info.

--- DwRaid/DwRaidCode.lua
+++ DwRaid/DwRaidCode.lua
@@ -6,280 +6,232 @@ buttons to make setting up and managing raid groups quicker and easier.
 
 --]]
 
+--Global Variables 
+appName = "|cFF0000FFDwGuild:|r ";
+
+local DwRaid = CreateFrame("FRAME") DwRaid:Hide()
+DwRaid:RegisterEvent("ADDON_LOADED")
+DwRaid:RegisterEvent("PLAYER_LOGIN")
+DwRaid:RegisterEvent("PLAYER_REGEN_DISABLED")
+DwRaid:RegisterEvent("PLAYER_REGEN_ENABLED")
+DwRaid:RegisterEvent("GROUP_JOINED")
+DwRaid:RegisterEvent("GROUP_ROSTER_UPDATE")
+
+function DwRaid:OnEvent(event,arg1,...)
+	if (event == "ADDON_LOADED" and arg1 == "DwRaid") then
+		
+		DwRaid:UnregisterEvent("ADDON_LOADED")
+		DwRaid_VARIABLES_LOADED();
+		
+--Functions that happen when Player Logs in or does Reload		
+	elseif (event == "PLAYER_LOGIN") then
+		EnableDisable();  
+		ChatFrame1:AddMessage("Hello ".. UnitName("Player").. " DwRaid has been Loaded!");
+		--[[This line should be commented by removing 1 " - " at the start, when not developing. Starts addon UI immediately on login for development 
+		DwRaid_Frame:Show();--]]
+
+--Hide the UI when entering Combat
+	elseif (event == "PLAYER_REGEN_DISABLED") then
+		if DwRaidConfigValues[DwRaidRealm][DwRaidChar].HideOnCombat then
+			if DwRaid_Frame:IsVisible() then
+				DwRaid_Frame:Hide();
+				ChatFrame1:AddMessage(appName.. "Entering Combat, DwRaid is now hidden type /DW to show again.");
+			end
+			if DwRaidOptionsFrame:IsVisible() then
+				DwRaidOptionsFrame:Hide();
+			end
+		end
+--[[Show when not in Combat
+	elseif (event == "PLAYER_REGEN_ENABLED") then
+		if DwRaidConfigValues[DwRaidRealm][DwRaidChar].AutoStart then
+			DwRaid_Frame:Show();
+			print(appName.."Not in combat")
+		end--]]
+
+--Show the UI when Joining a group and update the buttons text
+	elseif (event == "GROUP_JOINED") then
+		if DwRaidConfigValues[DwRaidRealm][DwRaidChar].Enabled then
+			if DwRaidConfigValues[DwRaidRealm][DwRaidChar].ShowWhenGrouped then
+				EnableDisable();
+				DwRaid_Frame:Show();
+			end
+		end
+		
+--Update buttons if group size changes
+	elseif (event == "GROUP_ROSTER_UPDATE") then
+		EnableDisable();
+	end
+	
+--Handle AutoStart
+	if DwRaidConfigValues[DwRaidRealm][DwRaidChar].AutoStart then
+		DwRaid_Frame:Show();
+	end
+end
+DwRaid:SetScript("OnEvent", DwRaid.OnEvent);
+
 --Register Slash Command to show UI
-SLASH_DOWNWIND1 = '/dw';
+SLASH_DOWNWIND1 = "/DWR";
+SLASH_DOWNWIND2 = "/DwRaid";
+SLASH_DOWNWIND3 = "/DW";
 function SlashCmdList.DOWNWIND(msg, editbox)
  EnableDisable();
  DwRaid_Frame:Show();
 end
-
---Functions that happen when Player Logs in or does Reload
-local LogInEventFrame = CreateFrame("Frame")
-LogInEventFrame:RegisterEvent("PLAYER_LOGIN")
-LogInEventFrame:SetScript("OnEvent", function(self,event,...) 
-	EnableDisable();  
-	DisableUnused();--Temporary disable buttons
-	ChatFrame1:AddMessage("Hello ".. UnitName("Player").. " DwRaid has been Loaded!");
-	DwRaid_VARIABLES_LOADED();
-	--[[This line should be commented by removing 1 " - " at the start, when not developing. Starts addon UI immediately on login for development 
-	DwRaid_Frame:Show();--]]
-end)
-
-
-
-
---Hide the UI when entering Combat
-local EnterCombatFrame = CreateFrame("Frame")
-EnterCombatFrame:RegisterEvent("PLAYER_REGEN_DISABLED")
-EnterCombatFrame:SetScript("OnEvent", function(self,event,...)
-	if DwRaidConfigValues[DwRaidRealm][DwRaidChar].HideOnCombat then
-		if DwRaid_Frame:IsVisible() then
-		DwRaid_Frame:Hide();
-		ChatFrame1:AddMessage("DwRaid- Entering Combat, DwRaid is now hidden type /dw to show again.");
-		end
-		if DwRaidOptionsFrame:IsVisible() then
-		DwRaidOptionsFrame:Hide();
-		end
-	end
 	
-end)
-
---Show the UI when Joining a group and update the buttons text
-local JoinGroupFrame = CreateFrame("Frame")
-JoinGroupFrame:RegisterEvent("GROUP_JOINED")
-JoinGroupFrame:SetScript("OnEvent", function(self,event,...)
-if DwRaidConfigValues[DwRaidRealm][DwRaidChar].Enabled then
-	if DwRaidConfigValues[DwRaidRealm][DwRaidChar].ShowWhenGrouped then
-		EnableDisable();
-		DwRaid_Frame:Show();
-	end
-end
-end)
-
-
-
-
 --Enable and Disable buttons based on group
 function EnableDisable()
 local DwRaid_inGroup = IsInGroup();
-if DwRaid_inGroup then
+	if DwRaid_inGroup then
+		DwRaid_FrameButtonRaidPartySwitch:Enable();
+		DwRaid_FrameButtonLootMethSwitch:Enable();
+		DwRaid_FrameButtonLootThreshold:Enable();
 
-DwRaid_FrameButtonRaidPartySwitch:Enable();
-DwRaid_FrameButtonLootMethSwitch:Enable();
-DwRaid_FrameButtonLootThreshold:Enable();
+		UpdateLootMethButton();
+		UpdateLootThresholdButton();
+		GroupButtonUpdate();
+	else
+		DwRaid_FrameButtonRaidPartySwitch:Disable();
+		DwRaid_FrameButtonRaidPartySwitch:SetText("Convert Group");
 
-UpdateLootMethButton();
-UpdateLootThresholdButton();
-GroupButtonUpdate();
+		DwRaid_FrameButtonLootMethSwitch:Disable();
+		DwRaid_FrameButtonLootMethSwitch:SetText("Loot Method");
 
-else
-DwRaid_FrameButtonRaidPartySwitch:Disable();
-DwRaid_FrameButtonRaidPartySwitch:SetText("Convert Group");
-
-DwRaid_FrameButtonLootMethSwitch:Disable();
-DwRaid_FrameButtonLootMethSwitch:SetText("Loot Method");
-
-DwRaid_FrameButtonLootThreshold:Disable();
-DwRaid_FrameButtonLootThreshold:SetText("Loot Threshold");
-
+		DwRaid_FrameButtonLootThreshold:Disable();
+		DwRaid_FrameButtonLootThreshold:SetText("Loot Threshold");
+	end
 end
-end
-
---Update buttons if group size changes
-local ButtonGroupFrame = CreateFrame("Frame")
-ButtonGroupFrame:RegisterEvent("GROUP_ROSTER_UPDATE")
-ButtonGroupFrame:SetScript("OnEvent", function(self,event,...)
-EnableDisable();
-end)
-
-
-
 
 --Update the Raid / Party Button Text and state
 function GroupButtonUpdate()
 local DwRaid_GroupSize
 local DwRaid_InRaid = IsInRaid();
-if DwRaidConfigValues[DwRaidRealm][DwRaidChar].GroupSizeDisplay then
-	if DwRaid_GroupSize ~= GetNumGroupMembers() then
-		DwRaid_GroupSize = GetNumGroupMembers();
-		ChatFrame1:AddMessage("DwRaid- Group Size: ".. DwRaid_GroupSize);
+	if DwRaidConfigValues[DwRaidRealm][DwRaidChar].GroupSizeDisplay then
+		if DwRaid_GroupSize ~= GetNumGroupMembers() then
+			DwRaid_GroupSize = GetNumGroupMembers();
+			ChatFrame1:AddMessage(appName.. "Group Size: ".. DwRaid_GroupSize);
+		end
 	end
-end
 
-if DwRaid_GroupSize <= 5 and DwRaid_InRaid then
-DwRaid_FrameButtonRaidPartySwitch:SetText("Make Party");
-else 
-DwRaid_FrameButtonRaidPartySwitch:SetText("Make Raid");
-end
+	if DwRaid_GroupSize <= 5 and DwRaid_InRaid then
+		DwRaid_FrameButtonRaidPartySwitch:SetText("Make Party");
+	else 
+		DwRaid_FrameButtonRaidPartySwitch:SetText("Make Raid");
+	end
 end
 
 --Change button function for Joining a Party or Raid
 function SwitchPartyRaid()
 local DwRaid_InRaid = IsInRaid();
 local DwRaid_inGroup = IsInGroup();
-if DwRaid_inGroup then
-if DwRaid_InRaid then
-ConvertToParty();
-GroupButtonUpdate();
-ChatFrame1:AddMessage("DwRaid- Converted group to a Party group 5-man");
-else 
-ConvertToRaid();
-GroupButtonUpdate();
-ChatFrame1:AddMessage("DwRaid- Converted group to a Raid group");
+	if DwRaid_inGroup then
+		if DwRaid_InRaid then
+			ConvertToParty();
+			GroupButtonUpdate();
+			ChatFrame1:AddMessage(appName.. "Converted group to a Party group 5-man");
+		else 
+			ConvertToRaid();
+			GroupButtonUpdate();
+			ChatFrame1:AddMessage(appName.. "Converted group to a Raid group");
+		end
+	end
 end
-end
-end
-
-
-
 
 --Update the Loot Method Button Text and State
 function UpdateLootMethButton()
-local DwRaid_method = GetLootMethod()
-
-if DwRaid_method == "freeforall" then
-DwRaid_FrameButtonLootMethSwitch:SetText("Free 4 All");
-end
-if DwRaid_method == "group" then
-DwRaid_FrameButtonLootMethSwitch:SetText("Group");
-end
-if DwRaid_method == "master" then
-DwRaid_FrameButtonLootMethSwitch:SetText("Master");
-end
-if DwRaid_method == "needbeforegreed" then
-DwRaid_FrameButtonLootMethSwitch:SetText("Need/Greed");
-end
-if DwRaid_method == "roundrobin" then
-DwRaid_FrameButtonLootMethSwitch:SetText("RoundRobin");
-end
-if DwRaid_method == "personal" then
-DwRaid_FrameButtonLootMethSwitch:SetText("personal");
-end
+	if GetLootMethod() == "freeforall" then	DwRaid_FrameButtonLootMethSwitch:SetText("Free 4 All");
+	elseif GetLootMethod() == "group" then DwRaid_FrameButtonLootMethSwitch:SetText("Group");
+	elseif GetLootMethod() == "master" then	DwRaid_FrameButtonLootMethSwitch:SetText("Master");
+	elseif GetLootMethod() == "needbeforegreed" then DwRaid_FrameButtonLootMethSwitch:SetText("Need/Greed");
+	elseif GetLootMethod() == "roundrobin" then	DwRaid_FrameButtonLootMethSwitch:SetText("RoundRobin");
+	elseif GetLootMethod() == "personalloot" then DwRaid_FrameButtonLootMethSwitch:SetText("Personal");
+	end
 end
 
 --Cycle through the Loot Methods and call for Button Update
 function CycleLootMeth()
-local DwRaid_method = GetLootMethod();
-
-local DwRaid_inGroup = IsInGroup();
-if DwRaid_inGroup then
-
-if DwRaid_method == "personal" then
-SetLootMethod("group")
-end
-if DwRaid_method == "group" then
-SetLootMethod("master", UnitName("Player"))
-end
-if DwRaid_method == "master" then
-SetLootMethod("needbeforegreed")
-end
-if DwRaid_method == "needbeforegreed" then
-SetLootMethod("roundrobin")
-end
-if DwRaid_method == "roundrobin" then
-SetLootMethod("freeforall")
-end
-if DwRaid_method == "freeforall" then
-SetLootMethod("personal")
+	if IsInGroup() then
+		if GetLootMethod() == "personalloot" then SetLootMethod("group");
+		elseif GetLootMethod() == "group" then SetLootMethod("master", UnitName("Player"));
+		elseif GetLootMethod() == "master" then SetLootMethod("needbeforegreed");
+		elseif GetLootMethod() == "needbeforegreed" then SetLootMethod("roundrobin");
+		elseif GetLootMethod() == "roundrobin" then SetLootMethod("freeforall");
+		elseif GetLootMethod() == "freeforall" then SetLootMethod("personalloot");
+		end
+	end
+	UpdateLootMethButton();
+	ChatFrame1:AddMessage(appName.. "Looting method changed to: ".. GetLootMethod());
 end
 
-UpdateLootMethButton();
-ChatFrame1:AddMessage("DwRaid- Looting method changed to: ".. method);
-end
-end
-
-
-
-
---[[ Loot Threshold Values
-2. Uncommon (green)
-3. Rare(blue)
-4. Epic (purple)
---]]
 --Update Loot Threshold Button
 function UpdateLootThresholdButton()
-local DwRaid_threshold = GetLootThreshold();
-
-if DwRaid_threshold == 2 then
-DwRaid_FrameButtonLootThreshold:SetText("Uncommon");
-end
-if DwRaid_threshold == 3 then
-DwRaid_FrameButtonLootThreshold:SetText("Rare");
-end
-if DwRaid_threshold == 4 then
-DwRaid_FrameButtonLootThreshold:SetText("Epic");
-end
-
+	if GetLootThreshold() == 2 then DwRaid_FrameButtonLootThreshold:SetText("Uncommon");
+	elseif GetLootThreshold() == 3 then DwRaid_FrameButtonLootThreshold:SetText("Rare");
+	elseif GetLootThreshold() == 4 then DwRaid_FrameButtonLootThreshold:SetText("Epic");
+	end
 end
 
 --Cycle through the Loot Thresholds
 function CycleLootThreshold()
-local DwRaid_threshold = GetLootThreshold();
-local DwRaid_inGroup = IsInGroup();
-if DwRaid_inGroup then
-
-if DwRaid_threshold == 2 then
-SetLootThreshold(3);
-ChatFrame1:AddMessage("DwRaid- Loot threshold set to: Rare - Blue quality items!");
+	if IsInGroup() then
+		if GetLootThreshold() == 2 then
+			SetLootThreshold(3);
+			ChatFrame1:AddMessage(appName.. "Loot threshold set to: Rare - Blue quality items!");
+		elseif GetLootThreshold() == 3 then
+			SetLootThreshold(4);
+			ChatFrame1:AddMessage(appName.. "Loot threshold set to: Epic - Purple quality items!");
+		elseif GetLootThreshold() == 4 then
+			SetLootThreshold(2);
+			ChatFrame1:AddMessage(appName.. "Loot threshold set to: Uncommon - Green quality items!");
+		end
+	end
+	UpdateLootThresholdButton();
 end
-if DwRaid_threshold == 3 then
-SetLootThreshold(4);
-ChatFrame1:AddMessage("DwRaid- Loot threshold set to: Epic - Purple quality items!");
-end
-if DwRaid_threshold == 4 then
-SetLootThreshold(2);
-ChatFrame1:AddMessage("DwRaid- Loot threshold set to: Uncommon - Green quality items!");
-end
-
-UpdateLootThresholdButton();
-end
-end
-
-
-
 
 --Pull Timer 
 function PullTimer()
 local PullTime = 10;
-local DwRaid_InRaid = IsInRaid();
-if DwRaid_InRaid then
-SendChatMessage("Starting 10 Second Pull Timer! Get Ready!", "RAID_WARNING");
-SendAddonMessage("BigWigs", "T:BWPull "..PullTime, "RAID");
-else
-SendChatMessage("Starting 10 Second Pull Timer! Get Ready!", "INSTANCE_CHAT"); --INSTANCE_CHAT 
-SendAddonMessage("BigWigs", "T:BWPull "..PullTime);
-end
-end
-
-
-
-
---Disable unused buttons "Temporary until finished"
-function DisableUnused()
-DwRaid_FrameButtonVoiceInfo:Disable();
-DwRaid_FrameButtonVoiceInfo:Hide();
-DwRaid_FrameButtonLootRulez:Disable();
-DwRaid_FrameButtonLootRulez:Hide();
-
-
+	if IsInRaid() then
+		SendChatMessage("Starting 10 Second Pull Timer! Get Ready!", "RAID_WARNING");
+		SendAddonMessage("BigWigs", "T:BWPull "..PullTime, "RAID");
+		
+	else
+		SendChatMessage("Starting 10 Second Pull Timer! Get Ready!", "Yell"); --INSTANCE_CHAT should change to instance when go live ?or maybe an option?
+		SendAddonMessage("BigWigs", "T:BWPull "..PullTime);
+	end
 end
 
+function LootRulesChatSend()
+	if DwRaidConfigValues[DwRaidRealm][DwRaidChar].LootRulesText ~= DwRaidConfig_defaultLootRulesText then
+		if IsInRaid() then
+			SendChatMessage(DwRaidConfigValues[DwRaidRealm][DwRaidChar].LootRulesText, "RAID");
+		elseif IsInGroup(LE_PARTY_CATEGORY_INSTANCE) then
+			SendChatMessage(DwRaidConfigValues[DwRaidRealm][DwRaidChar].LootRulesText, "INSTANCE_CHAT");
+		elseif IsInGroup() then
+			SendChatMessage(DwRaidConfigValues[DwRaidRealm][DwRaidChar].LootRulesText, "PARTY");
+		else
+			print(appName.. "You are not in a group!");
+			
+		end
+	else
+		print(appName.. "Loot Rules are not set up! Please click options to set Loot Rules chat text.")
+	end
+end
 
+function VoiceInfoChatSend()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	if DwRaidConfigValues[DwRaidRealm][DwRaidChar].VoiceInfoText ~= DwRaidConfig_defaultVoiceInfoText then
+		if IsInRaid() then
+			SendChatMessage(DwRaidConfigValues[DwRaidRealm][DwRaidChar].VoiceInfoText, "RAID");
+		elseif IsInGroup(LE_PARTY_CATEGORY_INSTANCE) then
+			SendChatMessage(DwRaidConfigValues[DwRaidRealm][DwRaidChar].VoiceInfoText, "INSTANCE_CHAT");
+		elseif IsInGroup() then
+			SendChatMessage(DwRaidConfigValues[DwRaidRealm][DwRaidChar].VoiceInfoText, "PARTY");
+		else
+			print(appName.. "You are not in a group!");
+			--print(appName.. "Testing: ".. DwRaidConfigValues[DwRaidRealm][DwRaidChar].VoiceInfoText);
+			--SendChatMessage(text, "SAY");
+		end
+	else
+		print(appName.. "Voice Info is not set up! Please click options to set Voice Info chat text.");
+	end
+end
--- DwRaid/DwRaidOptionsCode.lua
+++ DwRaid/DwRaidOptionsCode.lua
@@ -1,127 +1,94 @@
---Begin Options
- -- Global
- DwRaid_variablesLoaded = false;
-   -- for configuration saving
- DwRaidRealm = GetCVar("realmName");
- DwRaidChar = UnitName("player");
-   -- default options settings
- local DwRaidConfig_defaultEnabled = true; --Enable or Disable the addon?
- local DwRaidConfig_defaultHideOnCombat = true; -- Hide window when entering combat?
- local DwRaidConfig_defaultShowWhenGrouped = true;	-- Automatically show main frame when entering a group?
- local DwRaidConfig_defaultGroupSizeDisplay  = true; -- Display group size in chat window?
- local DwRaidConfig_defaultLootRulezText  = "Enter your Loot Rulez"; -- Text to be displayed in the Loot Rulez edit Text Box and chat spam.
- local DwRaidConfig_defaultVoiceInfoText  = "Enter your Voice Info: Vent, Mumble, Teamspeak...."; -- Text to be displayed in the Voice Info edit Text Box and chat spam.
-
-
-
+-- Global variables
+appName = "|cFF0000FFDwGuild:|r ";
+DwRaid_variablesLoaded = false;
+DwRaidRealm = GetCVar("realmName");
+DwRaidChar = UnitName("player");
+ 
+-- default options settings
+local DwRaidConfig_defaultLootRulesText  = "Enter your Loot Rules"; -- Text to be displayed in the Loot Rules edit Text Box and chat spam.
+local DwRaidConfig_defaultVoiceInfoText  = "Enter your Voice Info: Vent, Mumble, Teamspeak...."; -- Text to be displayed in the Voice Info edit Text Box and chat spam.
 
 function DwRaid_VARIABLES_LOADED()
 --Initialize SavedVariable
-if DwRaidConfigValues == nil then
-DwRaidConfigValues = {};
-end
+	if DwRaidConfigValues == nil then DwRaidConfigValues = {}; end
 --Initialize Realm and Character Saves
-if DwRaidConfigValues[DwRaidRealm] == nil then
-DwRaidConfigValues[DwRaidRealm] = {};
-end
-if DwRaidConfigValues[DwRaidRealm][DwRaidChar]  == nil then
-DwRaidConfigValues[DwRaidRealm][DwRaidChar] = {};
-end
-
+	if DwRaidConfigValues[DwRaidRealm] == nil then DwRaidConfigValues[DwRaidRealm] = {}; end
+	if DwRaidConfigValues[DwRaidRealm][DwRaidChar]  == nil then DwRaidConfigValues[DwRaidRealm][DwRaidChar] = {}; end
 --Load Options, Set to Default if Missing
-if DwRaidConfigValues[DwRaidRealm][DwRaidChar].Enabled == nil then
-DwRaidConfigValues[DwRaidRealm][DwRaidChar].Enabled = DwRaidConfig_defaultEnabled;
-end
-if DwRaidConfigValues[DwRaidRealm][DwRaidChar].HideOnCombat == nil then
-DwRaidConfigValues[DwRaidRealm][DwRaidChar].HideOnCombat = DwRaidConfig_defaultHideOnCombat;
-end
-if DwRaidConfigValues[DwRaidRealm][DwRaidChar].ShowWhenGrouped == nil then
-DwRaidConfigValues[DwRaidRealm][DwRaidChar].ShowWhenGrouped = DwRaidConfig_defaultShowWhenGrouped;
-end
-if DwRaidConfigValues[DwRaidRealm][DwRaidChar].GroupSizeDisplay == nil then
-DwRaidConfigValues[DwRaidRealm][DwRaidChar].GroupSizeDisplay = DwRaidConfig_defaultGroupSizeDisplay;
-end
-if DwRaidConfigValues[DwRaidRealm][DwRaidChar].LootRulezText == nil then
-DwRaidConfigValues[DwRaidRealm][DwRaidChar].LootRulezText = DwRaidConfig_defaultLootRulezText;
-end
-if DwRaidConfigValues[DwRaidRealm][DwRaidChar].VoiceInfoText == nil then
-DwRaidConfigValues[DwRaidRealm][DwRaidChar].VoiceInfoText = DwRaidConfig_defaultVoiceInfoText;
-end
-	
-DwRaid_variablesLoaded = true;
-DwRaid_ConfigChange();
-
+	if DwRaidConfigValues[DwRaidRealm][DwRaidChar].Enabled == nil then DwRaidConfigValues[DwRaidRealm][DwRaidChar].Enabled = true; end--Enable or Disable the addon?
+	if DwRaidConfigValues[DwRaidRealm][DwRaidChar].HideOnCombat == nil then DwRaidConfigValues[DwRaidRealm][DwRaidChar].HideOnCombat = true; end-- Hide window when entering combat?
+	if DwRaidConfigValues[DwRaidRealm][DwRaidChar].ShowWhenGrouped == nil then DwRaidConfigValues[DwRaidRealm][DwRaidChar].ShowWhenGrouped = true; end-- Automatically show main frame when entering a group?
+	if DwRaidConfigValues[DwRaidRealm][DwRaidChar].GroupSizeDisplay == nil then DwRaidConfigValues[DwRaidRealm][DwRaidChar].GroupSizeDisplay = true; end-- Display group size in chat window?
+	if DwRaidConfigValues[DwRaidRealm][DwRaidChar].AutoStart == nil then DwRaidConfigValues[DwRaidRealm][DwRaidChar].AutoStart = true; end-- Enable or Disable the addon AutoStarting?
+	if DwRaidConfigValues[DwRaidRealm][DwRaidChar].LootRulesText == nil then DwRaidConfigValues[DwRaidRealm][DwRaidChar].LootRulesText = DwRaidConfig_defaultLootRulesText; end
+	if DwRaidConfigValues[DwRaidRealm][DwRaidChar].VoiceInfoText == nil then DwRaidConfigValues[DwRaidRealm][DwRaidChar].VoiceInfoText = DwRaidConfig_defaultVoiceInfoText; end
+	DwRaid_variablesLoaded = true;
+	DwRaid_ConfigChange();
 end
 
-
-
-
- -- OnShow
- function DwRaidOptionsFrame_OnShow()
-        -- read settings and change check buttons
-        DwRaidOptionsFrameCheckButtonEnabled:SetChecked(DwRaidConfigValues[DwRaidRealm][DwRaidChar].Enabled);
-        DwRaidOptionsFrameCheckButtonHideOnCombat:SetChecked(DwRaidConfigValues[DwRaidRealm][DwRaidChar].HideOnCombat);           
-        DwRaidOptionsFrameCheckButtonShowWhenGrouped:SetChecked(DwRaidConfigValues[DwRaidRealm][DwRaidChar].ShowWhenGrouped);
-        DwRaidOptionsFrameCheckButtonGroupSizeDisplay:SetChecked(DwRaidConfigValues[DwRaidRealm][DwRaidChar].GroupSizeDisplay);  
-		
-		--ChatFrame1:AddMessage("Settings read");
-		
-		DwRaid_ConfigChange();
+function DwRaidOptionsFrame_OnShow()
+-- read settings and change check buttons
+    DwRaidOptionsFrameCheckButtonEnabled:SetChecked(DwRaidConfigValues[DwRaidRealm][DwRaidChar].Enabled);
+    DwRaidOptionsFrameCheckButtonHideOnCombat:SetChecked(DwRaidConfigValues[DwRaidRealm][DwRaidChar].HideOnCombat);           
+    DwRaidOptionsFrameCheckButtonShowWhenGrouped:SetChecked(DwRaidConfigValues[DwRaidRealm][DwRaidChar].ShowWhenGrouped);
+    DwRaidOptionsFrameCheckButtonGroupSizeDisplay:SetChecked(DwRaidConfigValues[DwRaidRealm][DwRaidChar].GroupSizeDisplay);
+	DwRaidOptionsFrameCheckButtonAutoStart:SetChecked(DwRaidConfigValues[DwRaidRealm][DwRaidChar].AutoStart);
+	LootRulesTextEditBox:SetText(DwRaidConfigValues[DwRaidRealm][DwRaidChar].LootRulesText);
+	VoiceInfoTextEditBox:SetText(DwRaidConfigValues[DwRaidRealm][DwRaidChar].VoiceInfoText);
+	--ChatFrame1:AddMessage("Settings read");
+	DwRaid_ConfigChange();
+end
+ 
+function DwRaidOptionsFrame_OnClick()
+--Update settings when user changes something
+    DwRaidConfigValues[DwRaidRealm][DwRaidChar].Enabled = DwRaidOptionsFrameCheckButtonEnabled:GetChecked(); 
+	DwRaidConfigValues[DwRaidRealm][DwRaidChar].HideOnCombat = DwRaidOptionsFrameCheckButtonHideOnCombat:GetChecked();
+	DwRaidConfigValues[DwRaidRealm][DwRaidChar].ShowWhenGrouped = DwRaidOptionsFrameCheckButtonShowWhenGrouped:GetChecked();
+	DwRaidConfigValues[DwRaidRealm][DwRaidChar].GroupSizeDisplay = DwRaidOptionsFrameCheckButtonGroupSizeDisplay:GetChecked();
+	DwRaidConfigValues[DwRaidRealm][DwRaidChar].AutoStart = DwRaidOptionsFrameCheckButtonAutoStart:GetChecked();
+	--ChatFrame1:AddMessage("click");
+    DwRaid_ConfigChange();
  end
  
- 
- 
- 
-  -- OnClick
- function DwRaidOptionsFrame_OnClick()
-      	--Update settings when user changes something
-		
-        DwRaidConfigValues[DwRaidRealm][DwRaidChar].Enabled = DwRaidOptionsFrameCheckButtonEnabled:GetChecked(); 
-		DwRaidConfigValues[DwRaidRealm][DwRaidChar].HideOnCombat = DwRaidOptionsFrameCheckButtonHideOnCombat:GetChecked();
-		DwRaidConfigValues[DwRaidRealm][DwRaidChar].ShowWhenGrouped = DwRaidOptionsFrameCheckButtonShowWhenGrouped:GetChecked();
-	    DwRaidConfigValues[DwRaidRealm][DwRaidChar].GroupSizeDisplay = DwRaidOptionsFrameCheckButtonGroupSizeDisplay:GetChecked();
-       		
-		--ChatFrame1:AddMessage("click");
-        DwRaid_ConfigChange();
+ function LootRulesAcceptBtnClick()
+ --Update when click save
+	DwRaidConfigValues[DwRaidRealm][DwRaidChar].LootRulesText = LootRulesTextEditBox:GetText();
+	DwRaidOptionsLootRulesFrame:Hide();
  end
  
- 
- 
-  function DwRaid_ConfigChange()
-        if DwRaidConfigValues[DwRaidRealm][DwRaidChar].Enabled then
-			--[[ChatFrame1:AddMessage("DwRaid- Enabled, showing main UI!");--]]
-			EnableDisable()
-			DwRaid_Frame:Show();
-        else
-            DwRaid_Frame:Hide();
-			--[[ChatFrame1:AddMessage("DwRaid- Disabled, hiding main UI!");--]]
-			
-        end
-		
-	
-		--[[ChatFrame1:AddMessage("DwRaid- Config Change!");--]]
-		
+ function VoiceInfoAcceptBtnClick()
+ 	DwRaidConfigValues[DwRaidRealm][DwRaidChar].VoiceInfoText = VoiceInfoTextEditBox:GetText();
+	DwRaidOptionsVoiceInfoFrame:Hide();
  end
  
+function DwRaid_ConfigChange()
+    if DwRaidConfigValues[DwRaidRealm][DwRaidChar].Enabled then
+		--[[
+		ChatFrame1:AddMessage("DwRaid- Enabled, showing main UI!");--]]
+		EnableDisable()
+		DwRaid_Frame:Show();
+    else
+        DwRaid_Frame:Hide();
+		--[[
+		ChatFrame1:AddMessage("DwRaid- Disabled, hiding main UI!");--]]
+	end
+	--[[ChatFrame1:AddMessage("DwRaid- Config Change!");--]]
+end
  
- 
- 
- -- reset to defaults
- function DwRaid_SetToDefault()
-        DwRaidConfigValues[DwRaidRealm][DwRaidChar].Enabled = DwRaidConfig_defaultEnabled;
-		DwRaidConfigValues[DwRaidRealm][DwRaidChar].HideOnCombat = DwRaidConfig_defaultHideOnCombat;
-		DwRaidConfigValues[DwRaidRealm][DwRaidChar].ShowWhenGrouped = DwRaidConfig_defaultShowWhenGrouped;
-		DwRaidConfigValues[DwRaidRealm][DwRaidChar].GroupSizeDisplay = DwRaidConfig_defaultGroupSizeDisplay;
-		DwRaidConfigValues[DwRaidRealm][DwRaidChar].LootRulezText = DwRaidConfig_defaultLootRulezText;
-		DwRaidConfigValues[DwRaidRealm][DwRaidChar].VoiceInfoText = DwRaidConfig_defaultVoiceInfoText;
+-- reset to defaults
+function DwRaid_SetToDefault()
+    DwRaidConfigValues[DwRaidRealm][DwRaidChar].Enabled = true;
+	DwRaidConfigValues[DwRaidRealm][DwRaidChar].HideOnCombat = true;
+	DwRaidConfigValues[DwRaidRealm][DwRaidChar].ShowWhenGrouped = true;
+	DwRaidConfigValues[DwRaidRealm][DwRaidChar].GroupSizeDisplay = true;
+	DwRaidConfigValues[DwRaidRealm][DwRaidChar].AutoStart = true;
+	DwRaidConfigValues[DwRaidRealm][DwRaidChar].LootRulesText = DwRaidConfig_defaultLootRulesText;
+	DwRaidConfigValues[DwRaidRealm][DwRaidChar].VoiceInfoText = DwRaidConfig_defaultVoiceInfoText;
 
-        -- Reset the location of the main frame
-        DwRaid_Frame:SetPoint("CENTER",0,0);
-		DwRaidOptionsFrame:SetPoint("CENTER",0,0);
-
-        DwRaid_ConfigChange();
-		DwRaidOptionsFrame_OnShow();
-		
-		ChatFrame1:AddMessage("DwRaid- Reset to Defualt settings!");
+    -- Reset the location of the main frame
+    DwRaid_Frame:SetPoint("CENTER",0,0);
+	DwRaidOptionsFrame:SetPoint("CENTER",0,0);
+    DwRaid_ConfigChange();
+	DwRaidOptionsFrame_OnShow();
+	ChatFrame1:AddMessage(appName.. "Settings have been reset to default!");
  end
-
